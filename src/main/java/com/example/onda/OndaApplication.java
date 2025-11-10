90ed30856716157c5931d9f65ed67f4104483177
babaprincedaouda71 <babadaouda01@gmail.com>
2025-11-10 16:55:34 +0100

    first commit

---
+++ src/main/java/com/example/onda/OndaApplication.java
@@ -0,0 +1,13 @@
+package com.example.onda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OndaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onda/OndaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
